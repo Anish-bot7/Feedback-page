f273a189e0a818053b22632da81cb54faa892648
Anish N S <anishsathavu@gmail.com>
2026-05-29 13:22:17 +0530

    fix port

--- FB-BE/Admin.js
+++ FB-BE/Admin.js
@@ -108,7 +108,7 @@ app.get('/user/:username/feedbacks', async (req, res) => {
 });
 
 // Start Server
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
